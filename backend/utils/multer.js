4ecffd7a4026edf44ead42b646ed3f7603134239
Nitin Kadam <nitinkadam.eminenture@gmail.com>
2024-05-14 13:07:38 +0530

    adding multer specification

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -6,15 +6,8 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (
-      ext !== ".jpg" &&
-      ext !== ".jpeg" &&
-      ext !== ".png" &&
-      ext !== ".webp"
-    ) {
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
+    cb(new Error("File type is not supported"), false);
+    return;
     cb(null, true);
   },
 });
